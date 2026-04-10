2574b511d365fd440a88545cbdcc88991cee1926
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2026-04-10 11:58:30 +0200

    Snapshot

---
+++ build.sh
@@ -0,0 +1,13 @@
+rm -f index.html 2>/dev/null
+touch index.html
+for i in caliptra-ss/*; do
+
+    echo "<a href=\"coverview.html?path=$i\">$i</a><br />" >> index.html
+done
+
+echo "<br />" >> index.html
+
+for i in i3c-core/*; do
+
+    echo "<a href=\"coverview.html?path=$i\">$i</a><br />" >> index.html
+done
